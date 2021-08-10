30508983fc9718d3856950a653ab9fcb14245d5e
!ereHyddeS <87564635+ereHyddeS@users.noreply.github.com>
2021-08-10 16:01:53 +0800

    Welp that didnt work

--- scripts/entities/bullet/DrainBulletType.java
+++ scripts/entities/bullet/DrainBulletType.java
@@ -14,6 +14,19 @@ import arc.math.*;
 public class DrainBulletType extends BulletType{
   public float drainStrength = 0.5f;
   
+      public SapBulletType(){
+        speed = 0f;
+        despawnEffect = Fx.none;
+        pierce = true;
+        collides = false;
+        hitSize = 0f;
+        hittable = false;
+        hitEffect = Fx.hitLiquid;
+        lightOpacity = 0.6f;
+        impact = true;
+    }
+
+  
       @Override
     public void init(Bullet b){
         super.init(b);
@@ -28,4 +41,16 @@ public class DrainBulletType extends BulletType{
                 h.heal(result * drainStrength);
             }
         }
+              if(target instanceof Hitboxc hit){
+            hit.collision(b, hit.x(), hit.y());
+            b.collision(hit, hit.x(), hit.y());
+        }else if(target instanceof Building tile){
+            if(tile.collide(b)){
+                tile.collision(b);
+                hit(b, tile.x, tile.y);
+            }
+        }else{
+            b.data = new Vec2().trns(b.rotation(), length).add(b.x, b.y);
+        }
     }
+}
