8402e36e36fdd10e434193ec7d5d9a35bddbedba
!ereHyddeS <87564635+ereHyddeS@users.noreply.github.com>
2021-08-10 15:27:35 +0800

    I dunno what I am doing ;-;

---
+++ scripts/entities/bullet/DrainBulletType.java
@@ -0,0 +1,31 @@
+package expandedindustries.entities.bullet;
+
+import arc.*;
+import arc.graphics.*;
+import arc.graphics.g2d.*;
+import arc.math.geom.*;
+import arc.util.*;
+import mindustry.content.*;
+import mindustry.entities.*;
+import mindustry.gen.*;
+import mindustry.graphics.*;
+import arc.math.*;
+
+public class DrainBulletType extends BulletType{
+  public float drainStrength = 0.5f;
+  
+      @Override
+    public void init(Bullet b){
+        super.init(b);
+
+        Healthc target = Damage.linecast(b, b.x, b.y, b.rotation(), length);
+        b.data = target;
+
+        if(target != null){
+            float result = Math.max(Math.min(target.health(), damage), 0);
+
+            if(b.owner instanceof Healthc h){
+                h.heal(result * sapStrength);
+            }
+        }
+    }
