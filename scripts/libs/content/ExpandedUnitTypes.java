2238c5dc58417de3d2a895a75ab8c25d37e7e8e1
!ereHyddeS <seanespiritu615@gmail.com>
2021-08-11 13:57:42 +0800

    pain 3

--- scripts/libs/content/ExpandedUnitTypes.java
+++ scripts/libs/content/ExpandedUnitTypes.java
@@ -25,56 +25,6 @@ import static arc.math.Angles.*;
 import static mindustry.Vars.*;
 
 public class ExpandedUnits implements ContentList{
-   //Steal from BetaMindy
-    private static Entry<Class<? extends Entityc>, Prov<? extends Entityc>>[] types = new Entry[]{
-            prov(CraeUnitEntity.class, CraeUnitEntity::new)
-    };
-
-    private static ObjectIntMap<Class<? extends Entityc>> idMap = new ObjectIntMap<>();
-
-    /**
-     * Internal function to flatmap {@code Class -> Prov} into an {@link Entry}.
-     * @author GlennFolker
-     */
-    private static <T extends Entityc> Entry<Class<T>, Prov<T>> prov(Class<T> type, Prov<T> prov){
-        Entry<Class<T>, Prov<T>> entry = new Entry<>();
-        entry.key = type;
-        entry.value = prov;
-        return entry;
-    }
-
-    /**
-     * Setups all entity IDs and maps them into {@link EntityMapping}.
-     * @author GlennFolker
-     */
-
-    private static void setupID(){
-        for(
-                int i = 0,
-                j = 0,
-                len = EntityMapping.idMap.length;
-
-                i < len;
-
-                i++
-        ){
-            if(EntityMapping.idMap[i] == null){
-                idMap.put(types[j].key, i);
-                EntityMapping.idMap[i] = types[j].value;
-
-                if(++j >= types.length) break;
-            }
-        }
-    }
-
-    /**
-     * Retrieves the class ID for a certain entity type.
-     * @author GlennFolker
-     */
-    public static <T extends Entityc> int classID(Class<T> type){
-        return idMap.get(type, -1);
-    }
-   
    @Override
    public void load(){
        
