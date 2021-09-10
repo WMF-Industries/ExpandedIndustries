14545e3c9f08ff1fa2530f0b7e93a839315e60e6
!ereHyddeS <seanespiritu615@gmail.com>
2021-09-10 21:19:11 +0800

    suahcs

--- scripts/units/booterfly.js
+++ scripts/units/booterfly.js
@@ -4,6 +4,16 @@ const refresh = require("libs/refresh")
 const pygmy = extendContent(UnitType, "pygmy", {});
 pygmy.constructor = () => extend(UnitEntity, {
 });
+
+const schaus = extendContent(UnitType, "schaus", {});
+schaus.constructor = () => extend(UnitEntity, {
+});
+
 Events.on(ClientLoadEvent, b  => {
    pygmy.abilities.add(new StatusFieldAbility(statuses.revitalizing, 50, 110, 0));
+   schaus.abilities.add(new StatusFieldAbility(statuses.revitalizing, 100, 170, 0));
+   schaus.weapons.get(0).bullet.status = StatusEffects.sapped;
+   schaus.weapons.get(0).bullet.statusDuration = 45;
+   schaus.weapons.get(0).bullet.fragBullet.status = StatusEffects.sapped;
+   schaus.weapons.get(0).bullet.fragBullet.statusDuration = 30;
 });
