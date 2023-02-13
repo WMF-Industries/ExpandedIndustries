3ee5436ad1e9c5c8e4e7560dd9608e7ba0e90c12
Minecriteo <100639255+Minecriteo@users.noreply.github.com>
2023-02-13 18:02:43 +0100

    Update LiquidOutputReactor.java

--- src/ExpandedIndustries/world/blocks/power/LiquidOutputReactor.java
+++ src/ExpandedIndustries/world/blocks/power/LiquidOutputReactor.java
@@ -2,6 +2,44 @@ package ExpandedIndustries.world.blocks.power;
 
 import mindustry.world.blocks.power.NuclearReactor;
 
+public class NuclearReactor {
+    private Liquid coolant;
+    private float coolantAmount;
+    private Liquid outputLiquid;
+    private float outputAmount;
+    private Liquid inputLiquid;
+    private float inputAmount;
+    private float maxInput;
+    private float maxOutput;
+    private float coolantLiquid; // custom float for coolant liquid
+
+    public NuclearReactor(Liquid coolant, float maxInput, float maxOutput, float coolantLiquid) {
+        this.coolant = coolant;
+        this.maxInput = maxInput;
+        this.maxOutput = maxOutput;
+        this.coolantLiquid = coolantLiquid;
+    }
+
+    public void update(Liquid inputLiquid, float inputAmount, float heat) {
+        this.inputLiquid = inputLiquid;
+        this.inputAmount = inputAmount;
+
+        float maxUsed = Math.min(heat / coolant.heatCapacity * coolantAmount, coolantAmount * coolantLiquid);
+        outputAmount = Math.min(maxOutput, maxUsed / outputLiquid.heatCapacity);
+
+        // update coolant amount
+        coolantAmount += inputAmount - outputAmount;
+
+        if (coolantAmount < 0) {
+            coolantAmount = 0;
+        }
+    }
+
+    public float getOutputAmount() {
+        return outputAmount;
+    }
+}
+
 public class LiquidReactor extends NuclearReactor {
     public LiquidReactor(String name) {
         super(name);
